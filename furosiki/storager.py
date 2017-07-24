9675a3c9839fe46ede88155095b973db9e75be15
奥田 隼渡 <oueiiti7@gmail.com>
2017-07-25 07:17:40 +0900

    テストディレクトリを作成

--- furosiki/storager.py
+++ furosiki/storager.py
@@ -1,19 +1,23 @@
+import os
 import numpy as np
 import dimen as dm
 import chunk as ck
 
 class Storager():
-  def save(self, chunk):
-    pos = chunk.pos
-    np.savez_compressed(
-      "%d_%d_%d.npz" % (pos[dm.X], pos[dm.Y], pos[dm.Z]),
-      chunk.cube)
+  def save(self, dir_path, chunk):
+    if not os.path.isdir(dir_path):
+      os.makedirs(dir_path)
 
-  def load(self, pos):
+    pos = chunk.pos
+    path = dir_path + "/%d_%d_%d.npz" % (pos[dm.X], pos[dm.Y], pos[dm.Z])
+
+    np.savez_compressed(path, chunk.cube)
+
+  def load(self, path, pos):
     pass
 
 if __name__ == '__main__':
 
   chunk = ck.create(1, 2, 3)
   storager = Storager()
-  storager.save(chunk)
+  storager.save("test_data", chunk)
