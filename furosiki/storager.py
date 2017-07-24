70ed129c04d7ca7aa891e61da8648f6ea1cd2e39
奥田 隼渡 <oueiiti7@gmail.com>
2017-07-25 07:10:14 +0900

    保存できた

--- furosiki/storager.py
+++ furosiki/storager.py
@@ -1,10 +1,13 @@
 import numpy as np
+import dimen as dm
 import chunk as ck
 
 class Storager():
   def save(self, chunk):
     pos = chunk.pos
-    np.savez_compressed("%d_%d_%d.npz" % (pos.x, pos.y, pos.z), chunk.cube)
+    np.savez_compressed(
+      "%d_%d_%d.npz" % (pos[dm.X], pos[dm.Y], pos[dm.Z]),
+      chunk.cube)
 
   def load(self, pos):
     pass
